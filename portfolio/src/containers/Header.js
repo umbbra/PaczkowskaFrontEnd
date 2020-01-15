381af80ce1723b01c16bad40f4327ddbaf22e46a
umbbra <umbra@spoko.pl>
2020-01-15 17:36:33 +0100

    finish rwd styles

--- portfolio/src/containers/Header.js
+++ portfolio/src/containers/Header.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import bgcHeader from '../images/tlo500.jpg';
 
 const Header = () => {
   return (
     <header className="header">
-      <img className="header__bgc" src={bgcHeader} alt="Background of Header" />
       <div className="header__description">
         <p className="header__description--name">Patrycja</p>
         <p className="header__description--surname">Paczkowska</p>
         <p className="header__description--job">Junior Front-End Developer</p>
       </div>
     </header>
-
   );
 };
 
--- portfolio/src/containers/Projects.js
+++ portfolio/src/containers/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import salonpiekna from "../images/salonpiekna.jpg";
 import portfolio from "../images/portfolio.jpg";
 import simplePage from "../images/simplePage.jpg";
-import CodeIcon from '@material-ui/icons/Code';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-
-
-
+import CodeIcon from "@material-ui/icons/Code";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 const Projects = () => {
   return (
@@ -15,26 +12,93 @@ const Projects = () => {
 
       <article className="projects__project">
         <div className="projects__project--about">
-          <p>Salon piękności Wenus to fikcyjna firma. Strona resposywna stworzona w JavaScript, Css i Flexboxie.</p>
-          <div className="projects__project--about__Boxlink"><a className="projects__project--about__Boxlink--link"href="https://umbbra.github.io/SalonPieknosci/"><VisibilityIcon /> Live</a></div>
-          
-          <a href="https://umbbra.github.io/SalonPieknosci/"><CodeIcon /> Code</a>
+          <p>
+            Venus Beauty Salon is a fictional company. A resposive website
+            created in JavaScript, Css and Flexbox
+          </p>
+          <div className="projects__project--about__boxlink">
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            >
+              <VisibilityIcon /> Live
+            </a>
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            > <CodeIcon /> Code
+            </a>
+          </div>
         </div>
         <img className="projects__project--img" src={salonpiekna} alt="sth" />
       </article>
 
       <article className="projects__project">
-        <div className="projects__project--about"></div>
+        <div className="projects__project--about">
+        <p>
+            Venus Beauty Salon is a fictional company. A resposive website
+            created in JavaScript, Css and Flexbox
+          </p>
+          <div className="projects__project--about__boxlink">
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            >
+              <VisibilityIcon /> Live
+            </a>
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            > <CodeIcon /> Code
+            </a>
+          </div>
+          </div>
         <img className="projects__project--img" src={simplePage} alt="sth" />
       </article>
 
       <article className="projects__project">
-        <div className="projects__project--about"></div>
+        <div className="projects__project--about">
+        <p>
+            Venus Beauty Salon is a fictional company. A resposive website
+            created in JavaScript, Css and Flexbox
+          </p>
+          <div className="projects__project--about__boxlink">
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            >
+              <VisibilityIcon /> Live
+            </a>
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            > <CodeIcon /> Code
+            </a>
+          </div>
+        </div>
         <img className="projects__project--img" src={portfolio} alt="sth" />
       </article>
 
       <article className="projects__project">
-        <div className="projects__project--about"></div>
+        <div className="projects__project--about">
+        <p>
+            Venus Beauty Salon is a fictional company. A resposive website
+            created in JavaScript, Css and Flexbox
+          </p>
+          <div className="projects__project--about__boxlink">
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            >
+              <VisibilityIcon /> LIVE
+            </a>
+            <a
+              className="projects__project--about__boxlink--link"
+              href="https://umbbra.github.io/SalonPieknosci/"
+            > <CodeIcon /> CODE
+            </a>
+          </div>
+        </div>
         <img className="projects__project--img" src={salonpiekna} alt="sth" />
       </article>
     </section>
