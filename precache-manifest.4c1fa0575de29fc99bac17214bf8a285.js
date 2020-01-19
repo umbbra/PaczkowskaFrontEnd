dbb87f55d1d34eed0bfe10ca1cd71d305d158b01
umbbra <umbra@spoko.pl>
2020-01-19 12:29:44 +0100

    Updates

---
+++ precache-manifest.4c1fa0575de29fc99bac17214bf8a285.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c95231727ac9e09cdbe4eae0014ae2",
+    "url": "/PaczkowskaFrontEnd/index.html"
+  },
+  {
+    "revision": "e1b77b186f3a31a3caeb",
+    "url": "/PaczkowskaFrontEnd/static/css/main.7d340d9f.chunk.css"
+  },
+  {
+    "revision": "9cf4f1511b090235fcbe",
+    "url": "/PaczkowskaFrontEnd/static/js/2.e28b6f91.chunk.js"
+  },
+  {
+    "revision": "a58d60b4914e1164d175ad7f57a8d682",
+    "url": "/PaczkowskaFrontEnd/static/js/2.e28b6f91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1b77b186f3a31a3caeb",
+    "url": "/PaczkowskaFrontEnd/static/js/main.d588a7eb.chunk.js"
+  },
+  {
+    "revision": "915137ff0324c8444769",
+    "url": "/PaczkowskaFrontEnd/static/js/runtime-main.e9622483.js"
+  },
+  {
+    "revision": "5710582f04b7f3b953d93ece4894b9dc",
+    "url": "/PaczkowskaFrontEnd/static/media/me.5710582f.jpg"
+  },
+  {
+    "revision": "40e778819be3314d8da740c3b85041db",
+    "url": "/PaczkowskaFrontEnd/static/media/portfolio.40e77881.jpg"
+  },
+  {
+    "revision": "1d3c80249fa0db25d0788e72743e583d",
+    "url": "/PaczkowskaFrontEnd/static/media/salonpiekna.1d3c8024.jpg"
+  },
+  {
+    "revision": "543bb3468f2b2468d8d64984d164469c",
+    "url": "/PaczkowskaFrontEnd/static/media/savings.543bb346.jpg"
+  },
+  {
+    "revision": "9fb60141de30b9a4d7f875271751a478",
+    "url": "/PaczkowskaFrontEnd/static/media/simplePage.9fb60141.jpg"
+  },
+  {
+    "revision": "a5bad2b2fc6b16b1aad4ec8cfee2a828",
+    "url": "/PaczkowskaFrontEnd/static/media/stardust.a5bad2b2.png"
+  },
+  {
+    "revision": "7b1c8bc2fb8d49bdb719c8f6f5652a74",
+    "url": "/PaczkowskaFrontEnd/static/media/tlo1200.7b1c8bc2.jpg"
+  },
+  {
+    "revision": "a25fad2b22978136be5c1a2ed2e48dff",
+    "url": "/PaczkowskaFrontEnd/static/media/tlo2000.a25fad2b.jpg"
+  },
+  {
+    "revision": "9140e75bcc8da09cd1a2a959119abc90",
+    "url": "/PaczkowskaFrontEnd/static/media/tlo4000.9140e75b.jpg"
+  },
+  {
+    "revision": "42b909736ab82ab5f10a34c2ae5fab79",
+    "url": "/PaczkowskaFrontEnd/static/media/tlo500.42b90973.jpg"
+  },
+  {
+    "revision": "6914b8c1cce9969f0e94329cf9388573",
+    "url": "/PaczkowskaFrontEnd/static/media/tlo800.6914b8c1.jpg"
+  },
+  {
+    "revision": "e8673615838b411882d06464152dc67f",
+    "url": "/PaczkowskaFrontEnd/static/media/todo.e8673615.jpg"
+  }
+]);
